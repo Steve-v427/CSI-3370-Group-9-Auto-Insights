51428cc6e8dc99d51b86859701d55c9b33769769
Steve-v427 <82559318+Steve-v427@users.noreply.github.com>
2023-11-27 18:40:32 -0500

    Delete MotorCityAuto directory

--- MotorCityAuto/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$con = mysqli_connect("localhost","root","root") or die(mysqli_error($con));
-		mysqli_query($con,"SET NAMES 'utf8'"); 
-		mysqli_query($con,'SET CHARACTER SET utf8'); 
-    $db = mysqli_select_db($con, "my_recipes") or die(mysqli_error($con));
- ?>
